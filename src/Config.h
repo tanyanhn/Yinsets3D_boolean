6a52b57136a256ce731555610aafb8df33d314cf
QiuyunhaoZJU <63397382+QiuyunhaoZJU@users.noreply.github.com>
2021-04-22 16:54:31 +0800

    triangulation

--- src/Config.h
+++ src/Config.h
@@ -9,6 +9,9 @@ namespace YSB
     const Real TOL = 10e-5;
     const int spaceDim = 3;
     const Real GreatValue = 10e10;
+    #ifndef M_PI
+    #define M_PI 3.14159265358979323846
+    #endif
 } // namespace YSB
 
 #endif // CONFIG_H
