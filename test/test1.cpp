64e9f5a7368083e42f578d16c7bf7066e8d75aab
yan <tanyanhn@qq.com>
2021-04-30 00:56:57 +0800

    Acoomplish code for simple test,
    Add ensureFace to make sure inputdata.

--- test/test1.cpp
+++ test/test1.cpp
@@ -13,13 +13,26 @@ TEST_CASE("monkey&&monkey", "[mm]")
     //        s2("D://github//Yinsets3D_boolean//dataset//icosphere//icosphere2.obj");
     // string smonkey1("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey1.obj"),
     //        smonkey2("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey2.obj");
-    string s1("D://github//Yinsets3D_boolean//dataset//teddy&cow//teddy.obj"),
-        s2("D://github//Yinsets3D_boolean//dataset//teddy&cow//cow.obj");
+    // string s1("D://github//Yinsets3D_boolean//dataset//teddy&cow//teddy.obj"),
+    //     s2("D://github//Yinsets3D_boolean//dataset//teddy&cow//cow.obj");
+    string name1 = "teddy",
+           name2 = "monkey",
+           inpre = "../dataset/",
+           oupre = "../res/",
+           post = ".obj",
+           mid = "teddy&monkey",
+           mid2 = "rabbit",
+           m = "meet/",
+           j = "join/",
+           c = "complement/";
+    string s1 = inpre + mid + "/" + name1 + post,
+           s2 = inpre + mid + "/" + name2 + post;
     YinSet<Real> y1 = importdata1(s1), y2 = importdata1(s2);
 
     auto res = y1.meet(y2);
-    exportdata("../res/cowteddymeet", res);
-    auto res2 = y1.join(y2);
-    exportdata("../res/cowteddyjoin", res2);
+    exportdata(name1 + "_" + name2, res,
+               oupre + name1 + "_" + name2 + "_" + m);
+    // auto res2 = y1.join(y2);
+    // exportdata("../res/cowteddyjoin", res2);
     REQUIRE(1 == 1);
 }
