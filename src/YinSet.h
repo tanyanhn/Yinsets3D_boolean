671d5801cea223c5596e85d303b812acf26766bc
yan <tanyanhn@qq.com>
2021-05-01 00:15:04 +0800

    Add mearsure performance.

--- src/YinSet.h
+++ src/YinSet.h
@@ -9,6 +9,9 @@
 #include "Locate.h"
 #include "PrePast.h"
 #include "Past.h"
+#ifdef _UsBoost
+#include <boost/timer/timer.hpp>
+#endif // _UsBoost
 
 namespace YSB
 {
@@ -86,22 +89,56 @@ namespace YSB
 
         YinSet<T> complement(const int usingRFB = 0, Real tol = TOL)
         {
+
             TriangleIntersection<T> intersectOp;
             std::vector<Triangle<T, 3>> inputA, inputB;
             collapse(inputA, 1, tol);
-            intersectOp(inputA, inputB, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "complement::TriangleIntersection cpu time : ";
+#endif // _UsBoost
+
+                intersectOp(inputA, inputB, tol);
+            }
 
             // Triangulation
             Triangulation<T> triangulateOp;
-            triangulateOp(inputA, inputB,
-                          intersectOp.resultA, intersectOp.resultB, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "complement::Triangulation cpu time : ";
+#endif // _UsBoost
+
+                triangulateOp(inputA, inputB,
+                              intersectOp.resultA, intersectOp.resultB, tol);
+            }
 
             // PrePast
             PrePast<T> prePastOp;
-            prePastOp(triangulateOp.vecTriA, tol);
-            for (auto &&tri : triangulateOp.vecTriA)
             {
-                tri.reverse();
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "complement::prePast cpu time : ";
+#endif // _UsBoost
+
+                prePastOp(triangulateOp.vecTriA, tol);
+            }
+
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "complement::reverse cpu time : ";
+#endif // _UsBoost
+
+                for (auto &&tri : triangulateOp.vecTriA)
+                {
+                    tri.reverse();
+                }
             }
             // std::vector<Triangle<T, 3>> *pVecTriA = &triangulateOp.vecTriA;
             // std::vector<Triangle<T, 3>> *pVecTriB = &triangulateOp.vecTriB;
@@ -125,13 +162,29 @@ namespace YSB
             // Past SurfacePatch to GluingCompactSurface.
             Past<T> pastOp;
             std::vector<SurfacePatch<T>> vecF;
-            pastOp.combine(prePastOp.vecSPA, prePastOp.vecSPB,
-                           triangulateOp.vecTriA, triangulateOp.vecTriB,
-                           vecF);
-            pastOp(vecF, triangulateOp.vecTriA, triangulateOp.vecTriB, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "complement::Past cpu time : ";
+#endif // _UsBoost
+
+                pastOp.combine(prePastOp.vecSPA, prePastOp.vecSPB,
+                               triangulateOp.vecTriA, triangulateOp.vecTriB,
+                               vecF);
+                pastOp(vecF, triangulateOp.vecTriA, triangulateOp.vecTriB, tol);
+            }
 
             // Yinset Constructor
-            return YinSet<T>(pastOp.vecGCS, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "complement::YinSet() cpu time : ";
+#endif // _UsBoost
+
+                return YinSet<T>(pastOp.vecGCS, tol);
+            }
         }
 
         YinSet<T> meet(YinSet<T> &y2, const int usingRFB = 0, Real tol = TOL)
@@ -139,39 +192,68 @@ namespace YSB
             //Intersection Triangle.
             TriangleIntersection<T> intersectOp;
             std::vector<Triangle<T, 3>> inputA, inputB;
-            collapse(inputA, 1, tol);
-            y2.collapse(inputB, 2, tol);
-            // for(int i=0;i<inputA.size();i++)
-            // {
-            //     T r1[3] = {-0.525730, -0.850652, 0.000000}, r2[3] = {-0.894426,-0.447216,0.000000}, r3[3] = {-0.688189, -0.525736, -0.499997};
-            //     Point<T, 3> tp1(r1), tp2(r2), tp3(r3);
-            //     Point<T, 3> tripoints1[] = {tp1, tp2, tp3};
-            //     Triangle<Real, 3> tri1(tripoints1);
-            //     if(tri1.equal(inputA[i]))
-            //     {
-            //         int a = i;
-            //     }
-            // }
-            //std::cout<<inputA.size()<<" "<<inputB.size()<<std::endl;
-            intersectOp(inputA, inputB, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "meet::collapse cpu time : ";
+#endif // _UsBoost
+
+                collapse(inputA, 1, tol);
+                y2.collapse(inputB, 2, tol);
+            }
+
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "meet::intersect cpu time : ";
+#endif // _UsBoost
+
+                intersectOp(inputA, inputB, tol);
+            }
 
             // Triangulation
             Triangulation<T> triangulateOp;
-            triangulateOp(inputA, inputB,
-                          intersectOp.resultA, intersectOp.resultB, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "meet::triangulate cpu time : ";
+#endif // _UsBoost
+
+                triangulateOp(inputA, inputB,
+                              intersectOp.resultA, intersectOp.resultB, tol);
+            }
 
             // RemoveOverlap
             RemoveOverlap<T> removeOverlapOp;
-            removeOverlapOp(triangulateOp.TriangulateA,
-                            triangulateOp.TriangulateB,
-                            triangulateOp.vecTriA,
-                            triangulateOp.vecTriB,
-                            intersectOp.resultA,
-                            intersectOp.resultB, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "meet::removeOverlap cpu time : ";
+#endif // _UsBoost
+
+                removeOverlapOp(triangulateOp.TriangulateA,
+                                triangulateOp.TriangulateB,
+                                triangulateOp.vecTriA,
+                                triangulateOp.vecTriB,
+                                intersectOp.resultA,
+                                intersectOp.resultB, tol);
+            }
 
             // PrePast
             PrePast<T> prePastOp;
-            prePastOp(triangulateOp.vecTriA, triangulateOp.vecTriB, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "meet::prePast cpu time : ";
+#endif // _UsBoost
+
+                prePastOp(triangulateOp.vecTriA, triangulateOp.vecTriB, tol);
+            }
 
             // std::vector<Triangle<T, 3>> *pVecTriA = &triangulateOp.vecTriA;
             // std::vector<Triangle<T, 3>> *pVecTriB = &triangulateOp.vecTriB;
@@ -185,27 +267,56 @@ namespace YSB
             // if (usingRFB == 1)
             // {
             //     ReFactoryBoundary<T> reFactoryBoundaryOp;
+            // {
+            //     boost::timer::auto_cpu_timer t;
+            //     std::cout << std::endl
+            //               << "meet::reFactoryBoundary cpu time : ";
             //     reFactoryBoundaryOp(triangulateOp.vecTriA, triangulateOp.vecTriB,
             //                         prePastOp.vecSPA, prePastOp.vecSPB,
             //                         prePastOp.ClipFaces, prePastOp.coClipFaces, tol);
             // }
+            // }
 
             // Locate SurfacePatch.
             Locate<T> locateOp;
-            locateOp(inputA, inputB, vecFace[0].Type(), y2.vecFace[0].Type(),
-                     triangulateOp.vecTriA, triangulateOp.vecTriB,
-                     prePastOp.vecSPA, prePastOp.vecSPB, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "meet::locate cpu time : ";
+#endif // _UsBoost
+
+                locateOp(inputA, inputB, vecFace[0].Type(), y2.vecFace[0].Type(),
+                         triangulateOp.vecTriA, triangulateOp.vecTriB,
+                         prePastOp.vecSPA, prePastOp.vecSPB, tol);
+            }
 
             // Past SurfacePatch to GluingCompactSurface.
             Past<T> pastOp;
             std::vector<SurfacePatch<T>> vecF;
-            pastOp.combine(prePastOp.vecSPA, prePastOp.vecSPB,
-                           triangulateOp.vecTriA, triangulateOp.vecTriB,
-                           vecF);
-            pastOp(vecF, triangulateOp.vecTriA, triangulateOp.vecTriB, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "meet::Past cpu time : ";
+#endif // _UsBoost
+
+                pastOp.combine(prePastOp.vecSPA, prePastOp.vecSPB,
+                               triangulateOp.vecTriA, triangulateOp.vecTriB,
+                               vecF);
+                pastOp(vecF, triangulateOp.vecTriA, triangulateOp.vecTriB, tol);
+            }
 
             // Yinset Constructor
-            return YinSet<T>(pastOp.vecGCS, tol);
+            {
+#ifdef _UsBoost
+                boost::timer::auto_cpu_timer t;
+                std::cout << std::endl
+                          << "meet::YinSet() cpu time : ";
+#endif // _UsBoost
+
+                return YinSet<T>(pastOp.vecGCS, tol);
+            }
         }
 
         YinSet<T> join(YinSet<T> &y2, const int usingRFB = 0, Real tol = TOL)
