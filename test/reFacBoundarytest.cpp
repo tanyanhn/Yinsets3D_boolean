3eefb0cd798f6a29f3d5a8b4856b34c873252a92
yan <tanyanhn@qq.com>
2021-06-19 19:31:11 +0800

    n

--- test/reFacBoundarytest.cpp
+++ test/reFacBoundarytest.cpp
@@ -5,68 +5,74 @@
 using namespace YSB;
 using namespace std;
 
-TEST_CASE("ball and torus 1", "[rF::bt1]") {
-  // string
-  // s1("D://github//Yinsets3D_boolean//dataset//icosphere//icosphere1.obj"),
-  //        s2("D://github//Yinsets3D_boolean//dataset//icosphere//icosphere2.obj");
-  // string
-  // smonkey1("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey1.obj"),
-  //        smonkey2("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey2.obj");
-  // string s1("D://github//Yinsets3D_boolean//dataset//teddy&cow//teddy.obj"),
-  //     s2("D://github//Yinsets3D_boolean//dataset//teddy&cow//cow.obj");
-  string name1 = "ballmid", name2 = "torusmid", inpre = "../dataset/",
-         oupre = "../res/",
-         //     inpre = "dataset/",
-      // oupre = "res/",
-      post = ".obj", mid = "icosphere", mid2 = "teddy&cow", mid3 = "rabbit",
-         mid4 = "heart", mid5 = "bust", mid6 = "ball", mid7 = "torus",
-         m = "meet/", j = "join/", c = "complement/";
-  string s1 = inpre + mid6 + "/" + name1 + post,
-         s2 = inpre + mid7 + "/" + name2 + post;
-  YinSet<Real> y1(s1), y2(s2);
-  auto y3 = y2.complement();
-  auto res = y1.meet(y3, 1);
+// TEST_CASE("ball and torus 1", "[rF::bt1]") {
+//   // string
+//   // s1("D://github//Yinsets3D_boolean//dataset//icosphere//icosphere1.obj"),
+//   // s2("D://github//Yinsets3D_boolean//dataset//icosphere//icosphere2.obj");
+//   // string
+//   //
+//   smonkey1("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey1.obj"),
+//   //
+//   smonkey2("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey2.obj");
+//   // string
+//   s1("D://github//Yinsets3D_boolean//dataset//teddy&cow//teddy.obj"),
+//   //     s2("D://github//Yinsets3D_boolean//dataset//teddy&cow//cow.obj");
+//   string name1 = "ballmid", name2 = "torusmid", inpre = "../dataset/",
+//          oupre = "../res/",
+//          //     inpre = "dataset/",
+//       // oupre = "res/",
+//       post = ".obj", mid = "icosphere", mid2 = "teddy&cow", mid3 = "rabbit",
+//          mid4 = "heart", mid5 = "bust", mid6 = "ball", mid7 = "torus",
+//          m = "meet/", j = "join/", c = "complement/";
+//   string s1 = inpre + mid6 + "/" + name1 + post,
+//          s2 = inpre + mid7 + "/" + name2 + post;
+//   YinSet<Real> y1(s1), y2(s2);
+//   auto y3 = y2.complement();
+//   auto res = y1.meet(y3, 1);
 
-  res.objOutput(name1 + "_" + name2, oupre + name1 + "_" + name2 + "_" + m);
+//   res.objOutput(name1 + "_" + name2, oupre + name1 + "_" + name2 + "_" + m);
 
-  REQUIRE(res.isBounded() == 1);
-  REQUIRE(res.getBettiNumber(0) == 1);
-  REQUIRE(res.getBettiNumber(1) == 1);
-  REQUIRE(1 == 1);
-}
+//   REQUIRE(res.isBounded() == 1);
+//   REQUIRE(res.getBettiNumber(0) == 1);
+//   REQUIRE(res.getBettiNumber(1) == 1);
+//   REQUIRE(1 == 1);
+// }
 
-TEST_CASE("torus contain 1 ball", "[rF::bt2]") {
-  // string
-  // s1("D://github//Yinsets3D_boolean//dataset//icosphere//icosphere1.obj"),
-  //        s2("D://github//Yinsets3D_boolean//dataset//icosphere//icosphere2.obj");
-  // string
-  // smonkey1("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey1.obj"),
-  //        smonkey2("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey2.obj");
-  // string s1("D://github//Yinsets3D_boolean//dataset//teddy&cow//teddy.obj"),
-  //     s2("D://github//Yinsets3D_boolean//dataset//teddy&cow//cow.obj");
-  string name1 = "ballleft", name2 = "torusbig", inpre = "../dataset/",
-         oupre = "../res/",
-         //     inpre = "dataset/",
-      // oupre = "res/",
-      post = ".obj", mid = "icosphere", mid2 = "teddy&cow", mid3 = "rabbit",
-         mid4 = "heart", mid5 = "bust", mid6 = "ball", mid7 = "torus",
-         m = "meet/", j = "join/", c = "complement/";
-  string s1 = inpre + mid6 + "/" + name1 + post,
-         s2 = inpre + mid7 + "/" + name2 + post;
-  YinSet<Real> y1(s1), y2(s2);
-  auto y3 = y1.complement();
-  auto res = y2.meet(y3, 1);
-  // auto res = y2.complement();
-  res.objOutput(name1 + "_" + name2, oupre + name1 + "_" + name2 + "_" + m);
-  // exportdata(name2, res,
-  //     oupre + name2 + "_" + c);
-  // auto res2 = y1.join(y2);
-  // exportdata("../res/cowteddyjoin", res2);
-  REQUIRE(res.isBounded() == 1);
-  REQUIRE(res.getBettiNumber(0) == 1);
-  REQUIRE(res.getBettiNumber(1) == 1);
-  REQUIRE(1 == 1);
-}
+// TEST_CASE("torus contain 1 ball", "[rF::bt2]") {
+//   // string
+//   // s1("D://github//Yinsets3D_boolean//dataset//icosphere//icosphere1.obj"),
+//   // s2("D://github//Yinsets3D_boolean//dataset//icosphere//icosphere2.obj");
+//   // string
+//   //
+//   smonkey1("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey1.obj"),
+//   //
+//   smonkey2("D://github//Yinsets3D_boolean//dataset//teddy&monkey//monkey2.obj");
+//   // string
+//   s1("D://github//Yinsets3D_boolean//dataset//teddy&cow//teddy.obj"),
+//   //     s2("D://github//Yinsets3D_boolean//dataset//teddy&cow//cow.obj");
+//   string name1 = "ballleft", name2 = "torusbig", inpre = "../dataset/",
+//          oupre = "../res/",
+//          //     inpre = "dataset/",
+//       // oupre = "res/",
+//       post = ".obj", mid = "icosphere", mid2 = "teddy&cow", mid3 = "rabbit",
+//          mid4 = "heart", mid5 = "bust", mid6 = "ball", mid7 = "torus",
+//          m = "meet/", j = "join/", c = "complement/";
+//   string s1 = inpre + mid6 + "/" + name1 + post,
+//          s2 = inpre + mid7 + "/" + name2 + post;
+//   YinSet<Real> y1(s1), y2(s2);
+//   auto y3 = y1.complement();
+//   auto res = y2.meet(y3, 1);
+//   // auto res = y2.complement();
+//   res.objOutput(name1 + "_" + name2, oupre + name1 + "_" + name2 + "_" + m);
+//   // exportdata(name2, res,
+//   //     oupre + name2 + "_" + c);
+//   // auto res2 = y1.join(y2);
+//   // exportdata("../res/cowteddyjoin", res2);
+//   REQUIRE(res.isBounded() == 1);
+//   REQUIRE(res.getBettiNumber(0) == 1);
+//   REQUIRE(res.getBettiNumber(1) == 1);
+//   REQUIRE(1 == 1);
+// }
 
 TEST_CASE("torus contain 2 balls", "[rF::bt3]") {
   // string
